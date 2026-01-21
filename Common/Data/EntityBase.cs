fcbf3b45c4eb249efaf08db84c26b7d3b5e22728
oldboni22 <oldboni22@gmail.com>
2026-01-21 16:17:22 +0300

    Removed redundant attributes

--- Common/Data/EntityBase.cs
+++ Common/Data/EntityBase.cs
@@ -5,8 +5,6 @@ namespace Common.Data;
 
 public class EntityBase
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id { get; set; }
     
     public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
