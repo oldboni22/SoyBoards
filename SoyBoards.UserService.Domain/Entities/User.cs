d81ac610d1b155ccc7be0318222dbe84ee227df8
oldboni22 <oldboni22@gmail.com>
2026-01-21 16:25:04 +0300

    Added MaxLength to User fields for the future db schema

--- SoyBoards.UserService.Domain/Entities/User.cs
+++ SoyBoards.UserService.Domain/Entities/User.cs
@@ -1,6 +1,8 @@
 #region
 
+using System.ComponentModel.DataAnnotations;
 using Common.Data;
+using SoyBoards.UserService.Domain.Constants;
 
 #endregion
 
@@ -8,9 +10,11 @@ namespace SoyBoards.UserService.Domain.Entities;
 
 public sealed class User : EntityBase
 {
+    [MaxLength(UserEntityConstants.MaxNameLength)]
     public required string DisplayName { get; set; }
     
+    [MaxLength(UserEntityConstants.MaxTagLength)]
     public required string Tag { get; init; }
-
+    
     public string? AvatarUrl { get; set; }
 }
