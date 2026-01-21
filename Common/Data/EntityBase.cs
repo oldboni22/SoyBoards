4816391260b10ffedbece127a51e2bf639b3c523
oldboni22 <oldboni22@gmail.com>
2026-01-21 16:21:01 +0300

    Fixed updated at type (removed nullable)

--- Common/Data/EntityBase.cs
+++ Common/Data/EntityBase.cs
@@ -6,5 +6,5 @@ public class EntityBase
     
     public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
     
-    public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
